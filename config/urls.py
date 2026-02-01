76f3e7f23b030af32d9c66edd7d02c901c260bff
Tiziano Montenegro <tizianomontenegro07@gmail.com>
2026-01-31 23:10:08 -0300

    Add API routes for flights and bookings, including JWT authentication endpoints

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from flights.views import FlightViewSet
+from bookings.views import BookingViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+router = DefaultRouter()
+router.register("flights", FlightViewSet)
+router.register("bookings", BookingViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/v1/auth/login/", TokenObtainPairView.as_view()),
+    path("api/v1/auth/refresh/", TokenRefreshView.as_view()),
+    path("api/v1/", include(router.urls)),
 ]
