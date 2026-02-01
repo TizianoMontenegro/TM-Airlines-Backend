61ac913f02ddaa03ababcb1fc04d40f3aeebc458
Tiziano Montenegro <tizianomontenegro07@gmail.com>
2026-01-31 23:09:55 -0300

    Add custom User model extending AbstractUser with unique email and language field

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+# User has the follow attributes
+# username, email, first_name, last_name,password, is_active, is_staff,
+# is_superuser, groups, user_permissions, last_login, date_joined language
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    language = models.CharField(max_length=5, default="en")
