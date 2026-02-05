a7358bd789bcd2620c8cfa0476d56faf91be9a94
Tiziano Montenegro <tizianomontenegro07@gmail.com>
2026-02-04 21:35:28 -0300

    Add admin interfaces for Airport and Flight models with search, filter, and ordering options

--- flights/admin.py
+++ flights/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Airport, Flight
 
 # Register your models here.
+@admin.register(Airport)
+class AirportAdmin(admin.ModelAdmin):
+    list_display = ("code", "city", "country")
+    search_fields = ("code", "city", "country")
+    ordering = ("code",)
+
+
+@admin.register(Flight)
+class FlightAdmin(admin.ModelAdmin):
+    list_display = (
+        "flight_number",
+        "origin",
+        "destination",
+        "departure_time",
+        "arrival_time",
+        "status",
+    )
+
+    list_filter = ("status", "origin", "destination")
+    search_fields = ("flight_number",)
+    ordering = ("departure_time",)
