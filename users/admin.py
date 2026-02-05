3379fb3a236b01f34aed970db3ea6cfbcd80b322
Tiziano Montenegro <tizianomontenegro07@gmail.com>
2026-02-04 21:35:49 -0300

    Add admin interface for custom User model with enhanced list display, filters, search options, and additional fieldsets

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    model = User
+
+    list_display = (
+        "id",
+        "username",
+        "email",
+        "is_staff",
+        "is_active",
+        "date_joined",
+    )
+
+    list_filter = ("is_staff", "is_active")
+
+    search_fields = ("username", "email")
+
+    ordering = ("-date_joined",)
+
+    fieldsets = UserAdmin.fieldsets + (
+        ("Preferences", {"fields": ("language",)}),
+    )
