b2f312b9f7c48138cecc5d2434871ff702317b31
Tiziano Montenegro <tizianomontenegro07@gmail.com>
2026-02-25 09:31:38 -0300

    Add temporary JWT authentication settings in config/settings.py, with access and refresh token lifetimes defined.

--- config/settings.py
+++ config/settings.py
@@ -132,3 +132,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+#### THIS CODE IS TEMPORARY PLACED 
+#### THIS SETTINGS HAS TO BE IMPORTED FROM base.py
+from datetime import timedelta
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
