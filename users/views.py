009cc4acd5237e0f6c06025a73b1a5e8f5f02893
Tiziano Montenegro <tizianomontenegro07@gmail.com>
2026-03-17 17:45:34 -0300

    Add RegistrationView for user registration, implementing API endpoint to create users with success response.

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .serializers import UserRegistrationSerializer
 
-# Create your views here.
+
+class RegistrationView(generics.CreateAPIView):
+    serializer_class = UserRegistrationSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(
+            {"message": "User created successfully."},
+            status=status.HTTP_201_CREATED
+        )
