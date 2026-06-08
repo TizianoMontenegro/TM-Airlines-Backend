1de73cb4174963953354fc10ecf8248db6aef9c5
Tiziano Montenegro <tizianomontenegro07@gmail.com>
2026-06-08 19:35:12 -0300

    Enhance BookingViewSet to support service mode filtering and add JWT authentication

--- bookings/views.py
+++ bookings/views.py
@@ -1,5 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from core.authentication import ServiceTokenAuthentication
 from .models import Booking
 from .serializers import BookingSerializer
 
@@ -8,10 +10,17 @@ from .serializers import BookingSerializer
 class BookingViewSet(ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    authentication_classes = [ServiceTokenAuthentication, JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        return Booking.objects.filter(user=self.request.user)
+        user = self.request.user
+        if getattr(self.request, "service_mode", False):
+            user_id = self.request.query_params.get("user_id")
+            if user_id:
+                return Booking.objects.filter(user_id=user_id)
+            return Booking.objects.all()
+        return Booking.objects.filter(user=user)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
