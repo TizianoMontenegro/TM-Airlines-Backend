aefecb70830d2a58e196b47ee8dc6f55388dbac0
Tiziano Montenegro <tizianomontenegro07@gmail.com>
2026-06-16 09:42:08 -0300

    Refactor production database settings to use dj-database-url for improved configuration. Added support for DATABASE_URL and included commented alternative for self-hosted PostgreSQL setup.

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,4 +1,5 @@
 from .base import *
+import dj_database_url
 import os
 
 DEBUG = os.getenv("DEBUG", "False") == "True"
@@ -6,15 +7,24 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
+
+# Self-hosted PostgreSQL alternative (uncomment and comment block above):
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.getenv("DB_NAME"),
+#         "USER": os.getenv("DB_USER"),
+#         "PASSWORD": os.getenv("DB_PASSWORD"),
+#         "HOST": os.getenv("DB_HOST"),
+#         "PORT": os.getenv("DB_PORT"),
+#     }
+# }
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 if not SECRET_KEY:
