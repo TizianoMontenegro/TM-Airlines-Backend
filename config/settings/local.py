a8f4bf08502cff750a7e729959176abd54573f1d
Tiziano Montenegro <tizianomontenegro07@gmail.com>
2026-06-18 21:01:57 -0300

    Refactor local database settings to support DATABASE_URL for PostgreSQL configuration. Implemented conditional logic to switch between PostgreSQL and SQLite based on the environment variable, enhancing flexibility for development and production environments.

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,10 +1,19 @@
 from .base import *
+import dj_database_url
+import os
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+DATABASE_URL = os.getenv("DATABASE_URL", "")
+
+if DATABASE_URL.startswith("postgres"):
+    DATABASES = {
+        "default": dj_database_url.config(default=DATABASE_URL, ssl_require=True),
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 DEBUG = True
